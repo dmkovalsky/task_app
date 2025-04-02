1b3895f372601d947c0b14a8e6c0a9ae1ae1b572
dmkovalsky <dmitrykovalsky@gmail.com>
2025-04-02 12:44:10 +0200

    solved task 4.5.2.4

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -4,6 +4,43 @@ class S4524 extends StatelessWidget {
   const S4524({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ShowHideNameWidget();
+  }
+}
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key});
+
+  @override
+  State<ShowHideNameWidget> createState() => _ShowHideNameWidgetState();
+}
+
+class _ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  String name = '';
+  String buttonValue = 'Name anzeigen';
+  bool isChanged = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 10,
+      children: [
+        Text(name),
+        ElevatedButton(
+            onPressed: () {
+              isChanged = !isChanged;
+              setState(() {
+                if (isChanged == true) {
+                  name = 'Dmitry';
+                  buttonValue = 'Name verstecken';
+                } else {
+                  name = '';
+                  buttonValue = 'Name anzeigen';
+                }
+              });
+            },
+            child: Text(buttonValue))
+      ],
+    );
   }
 }
