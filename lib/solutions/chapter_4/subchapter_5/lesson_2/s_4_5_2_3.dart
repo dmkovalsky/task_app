e2e3e5ed1cc15d32c4300e19e7d75076da5f76a5
dmkovalsky <dmitrykovalsky@gmail.com>
2025-04-02 12:13:38 +0200

    solved 4.5.2.3

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -4,6 +4,34 @@ class S4523 extends StatelessWidget {
   const S4523({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyNameWidget();
+  }
+}
+
+class MyNameWidget extends StatefulWidget {
+  const MyNameWidget({super.key});
+
+  @override
+  State<MyNameWidget> createState() => _MyNameWidgetState();
+}
+
+class _MyNameWidgetState extends State<MyNameWidget> {
+  String myTextContent = '';
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 10,
+      children: [
+        Text(myTextContent),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                myTextContent = 'Dmitry';
+              });
+              ;
+            },
+            child: Text('Name anzeigen')),
+      ],
+    );
   }
 }
