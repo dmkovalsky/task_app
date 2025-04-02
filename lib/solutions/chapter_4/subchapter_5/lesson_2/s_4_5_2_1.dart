f9590d977988abb0356002f011dc9969d7faddc3
dmkovalsky <dmitrykovalsky@gmail.com>
2025-04-02 11:07:36 +0200

    solved task 4.5.2.1

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
@@ -4,6 +4,15 @@ class S4521 extends StatelessWidget {
   const S4521({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return HelloWorldCentered();
+  }
+}
+
+class HelloWorldCentered extends StatelessWidget {
+  const HelloWorldCentered({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: const Text('Hello World'));
   }
 }
