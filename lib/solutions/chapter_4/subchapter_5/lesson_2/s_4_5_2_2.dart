8aacddcfc6b82ab8c21dba6eae6c14ebfcf0d50a
dmkovalsky <dmitrykovalsky@gmail.com>
2025-04-02 11:20:18 +0200

    solved task 4.5.2.2

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,20 @@ class S4522 extends StatelessWidget {
   const S4522({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyStateLessWidget();
+  }
+}
+
+class MyStateLessWidget extends StatelessWidget {
+  const MyStateLessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text('Hello Dmitry'),
+        ElevatedButton(onPressed: null, child: Text('Klick mich'))
+      ],
+    );
   }
 }
