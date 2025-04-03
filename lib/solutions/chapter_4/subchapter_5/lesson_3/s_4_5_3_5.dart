ab216ed8659c09baae13d2462b5d0b5a4bb705b9
dmkovalsky <dmitrykovalsky@gmail.com>
2025-04-03 14:20:55 +0200

    solved task 4.5.3.5

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
@@ -4,6 +4,50 @@ class S4535 extends StatelessWidget {
   const S4535({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyMosaikWidget();
+  }
+}
+
+class MyMosaikWidget extends StatelessWidget {
+  const MyMosaikWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RowColorBox(color: Colors.red),
+          RowColorBox(color: Colors.green)
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RowColorBox(color: Colors.blue),
+          RowColorBox(color: Colors.yellow),
+        ],
+      )
+    ]);
+  }
+}
+
+// TODO - move to file, if necessary can make new class for row for nice code :)
+class RowColorBox extends StatelessWidget {
+  // Attributes
+  final Color color;
+
+  const RowColorBox({
+    super.key,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      color: color,
+    );
   }
 }
