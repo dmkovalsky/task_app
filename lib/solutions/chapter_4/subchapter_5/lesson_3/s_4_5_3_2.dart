0a8e04cc5fe0c747151aac3dbe98682b51333484
dmkovalsky <dmitrykovalsky@gmail.com>
2025-04-03 13:36:43 +0200

    solved task 4.5.3.2

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_2.dart
@@ -4,6 +4,55 @@ class S4532 extends StatelessWidget {
   const S4532({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyWidget();
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 20,
+      children: [
+        CustomText(text: 'App Akademie'),
+        CustomText(
+            text: 'App Akademie',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+            )),
+        CustomText(
+            text: 'App Akademie',
+            style: TextStyle(
+              fontSize: 10,
+              fontStyle: FontStyle.italic,
+              color: Colors.green,
+            )),
+      ],
+    );
+  }
+}
+
+// TODO move CustomText to file
+class CustomText extends StatelessWidget {
+  // Parameters
+  final String text;
+  final TextStyle? style;
+
+  const CustomText({
+    super.key,
+    this.style,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: style,
+    );
   }
 }
