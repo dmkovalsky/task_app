7c31b8fbe7b81a4d26216c3d3fd9bb8a5d4ebff8
dmkovalsky <dmitrykovalsky@gmail.com>
2025-04-03 13:53:53 +0200

    solved task 4.5.3.4

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_4.dart
@@ -4,6 +4,24 @@ class S4534 extends StatelessWidget {
   const S4534({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyRowWidget();
+  }
+}
+
+class MyRowWidget extends StatelessWidget {
+  const MyRowWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      // spacing: 64, thats would be better choice :)
+      children: [
+        Text('App'),
+        SizedBox(
+          width: 64,
+        ),
+        Text('Akademie'),
+      ],
+    );
   }
 }
