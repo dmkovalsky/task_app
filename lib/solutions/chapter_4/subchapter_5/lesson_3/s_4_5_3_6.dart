cda5e94530b50527b6e9c122578897218b3dbfba
dmkovalsky <dmitrykovalsky@gmail.com>
2025-04-09 12:51:24 +0200

    solved task 4.5.3.6

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -1,9 +1,45 @@
 import 'package:flutter/material.dart';
 
+class Product {
+  final String name;
+  final double price;
+  final IconData icon;
+
+  Product({required this.name, required this.price, required this.icon});
+}
+
+List<Product> productList = [
+  Product(icon: Icons.tv, name: 'Fernseher', price: 279.95),
+  Product(icon: Icons.laptop_mac, name: 'Monitor', price: 169.99),
+  Product(icon: Icons.microwave, name: 'Mikrowelle', price: 129.99),
+  Product(icon: Icons.kitchen, name: 'Kühlschrank', price: 532.61),
+  Product(icon: Icons.microwave, name: 'Backofen', price: 349.99),
+];
+
 class S4536 extends StatelessWidget {
   const S4536({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ListView.separated(
+      shrinkWrap: true,
+      itemCount: productList.length,
+      itemBuilder: (context, index) {
+        Product currentProduct = productList[index];
+        return Card(
+            color: const Color.fromARGB(255, 176, 238, 192),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: ListTile(
+                leading: Icon(currentProduct.icon),
+                title: Text(currentProduct.name),
+                subtitle: Text(currentProduct.price.toString()),
+                trailing: Icon(Icons.shopping_cart_rounded),
+              ),
+            ));
+      },
+      separatorBuilder: (context, index) => SizedBox(
+        height: 20,
+      ),
+    );
   }
 }
